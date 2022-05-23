08214129edf6b99e30da55ff5559b239e0ede5d0
sergio1023 <sergyfutbol37@gmail.com>
2022-05-23 18:08:12 +0200

    procesar el radio y bd

---
+++ ejerciciosphp/formularioBD/formulariobd.php
@@ -0,0 +1,82 @@
+<?php
+print_r($_POST);
+
+$ip = "";
+$aula = "";
+$amd = "";
+$intel = "";
+$android = "";
+$formulario ="";
+
+$error = false;
+$errorip = false;
+$erroraula = false;
+$errorformulario = false;
+
+if (isset($_POST['enviar'])) {
+    if(isset($_POST['ip']) &&
+    $_POST['ip'] != ''){
+       $ip = $_POST['ip'];
+    }else{$errorip = true;}
+
+    if(isset($_POST['aula']) &&
+    $_POST['aula'] != ''){
+       $aula = $_POST['aula'];
+    } else {$erroraula = true;}
+
+    if(isset($_POST['tipo']) &&
+        $_POST['tipo'] != ''){
+          $formulario =  $_POST['tipo'];
+    }else {$errorformulario = true;}
+
+}
+
+include('acceso_datos.php');
+
+$datos = $conexion_bd->prepare("SELECT ip, aula, equipo FROM clase");
+$insertar = $conexion_bd->prepare("INSERT INTO clase VALUES (<?=$ip?>,<?=$aula?>,<?=$formulario?>)");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="errores.css" rel="stylesheet" type="text/css">
+    <title>Formulario Aula</title>
+</head>
+<body>
+    <?php if ($error == false) {  ?>
+    <h1>Registro centro</h1>
+    <form action="formulariobd.php" method="post">
+        IP<input   <?=($errorip)?'class="error"':'';?> type="text" name="ip" value="<?=$ip?>" placeholder="escribe la IP del aula"><br><br>
+        AULA<input   <?=($erroraula)?'class="error"':'';?> type="text" name="aula" value="<?=$aula?>" placeholder="Rellena con nombre de aula"><br><br>
+       Tipo de equipo: 
+        <input type="radio" name="tipo" value="intel" <?=$intel?>  <?=($urgente == 'intel')?'checked':''?>><span class="<?=($errorformulario)?'error':''?>">INTEL</span>
+        <input type="radio" name="tipo" value="amd"  <?=$amd?>  <?=($urgente == 'amd')?'checked':''?>><span class="<?=($errorformulario)?'error':''?>">AMD</span>
+        <input type="radio" name="tipo" value="android"  <?=$android?>  <?=($urgente == 'android')?'checked':''?>><span class="<?=($errorformulario)?'error':''?>">ANDROID</span><br><br>
+        <input type="submit" name="enviar" value="enviar">
+    </form>
+    <?php } else { ?>
+        <h2> información correcta </h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>IP</th>
+                    <th>AULA</th>
+                    <th>TIPO DE EQUIPO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($datos as $fila){ ?>
+                <tr>
+                    <td><?=$fila['ip']?></td>
+                    <td><?=$fila['aula']?></td>
+                    <td><?=$fila['equipo']?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    <?php } ?>
+</body>
+</html>
